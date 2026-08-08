1b0221563c42484f20d9022384fb4980c287e2dd
SahilDange14 <119867452+greedyraccoon@users.noreply.github.com>
2026-08-09 00:41:43 +0530

    fix(frontend): add base path for worker routing

--- Frontend/src/router.tsx
+++ Frontend/src/router.tsx
@@ -7,6 +7,7 @@ export const getRouter = () => {
 
   const router = createRouter({
     routeTree,
+    basepath: '/dreamestate',
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
